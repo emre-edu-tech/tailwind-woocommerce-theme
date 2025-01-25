f8ef3c0585f15be616bcf27e0208326129132195
Emre Kayikcilar <emreebrukayikcilar@gmail.com>
2025-01-25 09:26:58 +0300

    Theme creation started over

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Main Template</title>
+</head>
+<body>
+    Hello from main template!
+</body>
+</html>
